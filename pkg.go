6cfb821896afb47db4fc0d2e439f8913b3ff6119
Tymoteusz Makowski <tymek.makowski@gmail.com>
2021-10-13 21:49:17 +0200

    fix: start enum at 1

--- pkg.go
+++ pkg.go
@@ -5,7 +5,7 @@ type Status int
 
 const (
 	// Ordered Status means that an order has been placed.
-	Ordered Status = iota
+	Ordered Status = iota + 1
 	// Shipped Status means that a package has been posted.
 	Shipped
 	// Delivered Status means that a package has been delivered.
