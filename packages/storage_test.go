34d2d8cd2102ba24dd1ec1aa5cdbda462560a821
Tymoteusz Makowski <tymek.makowski@gmail.com>
2022-11-21 22:41:31 +0100

    test: add missing argument

--- packages/storage_test.go
+++ packages/storage_test.go
@@ -13,14 +13,14 @@ func TestDBStorage_NewDBStorage(t *testing.T) {
 	db, err := sql.Open("sqlite", ":memory:")
 	require.NoError(t, err)
 
-	_, err = NewDBStorage(db)
+	_, err = NewDBStorage(db, false)
 	assert.EqualError(t, err, "table not found: Packages(ID, Name, Inpost, Status)")
 
 	const query = "CREATE TABLE Packages(ID, Name, Inpost, Status)"
 	_, err = db.Exec(query)
 	require.NoError(t, err)
 
-	dbs, err := NewDBStorage(db)
+	dbs, err := NewDBStorage(db, false)
 	require.NoError(t, err)
 	assert.Equal(t, db, dbs.db)
 }
